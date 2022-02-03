57456d80d262b89fa311d96cc4a5ee7afa610dbe
J1min <jmhong101506@gmail.com>
2022-02-04 02:00:59 +0900

    오늘은 공지사항 표까지(버튼 안함)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Callender from "./Components/Callender";
+import Timetable from "./Components/Timetable";
 export default function App() {
   return (
     <div>
       <Callender />
+      <Timetable />
     </div>
   );
 }
--- src/Components/style.js
+++ src/Components/style.js
@@ -3,14 +3,12 @@ import styled from "styled-components";
 export const Month = styled.span`
   font-size: 24px;
 `;
-
 export const Day = styled.span`
   margin-top: 10px;
   margin-left: 14px;
   font-size: 13px;
   font-weight: bold;
 `;
-
 export const Date = styled.div`
   width: 118px;
   height: 118px;
@@ -22,7 +20,6 @@ export const Date = styled.div`
   box-shadow: 3px 3px 3px 3px #e1e1e1;
 `;
 // background-color: rgb(197, 197, 197);
-
 export const Today = styled.div`
   width: 118px;
   height: 118px;
@@ -34,3 +31,41 @@ export const Today = styled.div`
   box-shadow: 3px 3px 3px 3px #e1e1e1;
 `;
 // background-color: rgb(197, 197, 197);
+// ----------------------- Timetable --------------------
+
+export const Schedule = styled.div`
+  width: 700px;
+  height: 1080px;
+`;
+export const ScheduleTitle = styled.span`
+  display: block;
+  font-size: 36px;
+  font-weight: bold;
+  color: #ae6fff;
+  text-align: center;
+`;
+export const ScheduleBoard = styled.div`
+  width: 30%;
+  height: 750px;
+  border-radius: 10px;
+  background-color: #c8c8c8;
+`;
+export const ScheduleList = styled.li`
+  font-size: 18px;
+  font-weight: bold;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+  -ms-text-overflow: ellipsis;
+`;
+
+export const TodayTitle = styled.span`
+  display: block;
+  padding-top: 12px;
+  margin-bottom: 21px;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+  color: #ae6fff;
+`;
